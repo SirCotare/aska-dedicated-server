b45199435ca8d1973b01739bb144002e52689469
struppinet <you@example.com>
2025-11-23 16:16:41 +0100

    fix env var replace

--- files/env2cfg.sh
+++ files/env2cfg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-APP_FILE="$server_files/server properties.txt"
+APP_FILE="$savegame_files/my_server_properties.txt"
 
 variables=(
     "SESSION_NAME" "display name"
@@ -21,7 +21,7 @@ for ((i=0; i<${#variables[@]}; i+=2)); do
     if [ ! -z "${!var_name}" ]; then
         echo "${config_name} set to: ${!var_name}"
         if grep -q "$config_name" "$APP_FILE"; then
-            sed -i "/$config_name = /c $config_name = ${!var_name}" "$APP_FILE"
+            sed -i "s|^$config_name =.*|$config_name = ${!var_name}|" "$APP_FILE"
         else
             echo -ne "\n$config_name = ${!var_name}" >> "$APP_FILE"
         fi
